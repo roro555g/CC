e8bdd20ba6247decafd5dbecdd8dcf9f3d97eae1
Anderson Petry <devp3try@gmail.com>
2025-03-28 23:08:50 -0300

    Added hello_world.c

---
+++ Courses/C/Class01/hello_world.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    // Codigo
+    
+    printf("Hello World\n");
+    
+    return 0; // o return 0; exclama que o programa retornou um sucesso, caso o numero seja diferente, sera sinalizado que o retorno foi diferente.
+}
--- Courses/C/Class01/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    printf("Compiled program\n");
-    
-    return 0;
-}
