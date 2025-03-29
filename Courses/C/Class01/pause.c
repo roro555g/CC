0b7b19493510858a64d96e45ee0a3901d42605d3
Anderson Petry <devp3try@gmail.com>
2025-03-28 23:08:50 -0300

    Added hello_world.c

---
+++ Courses/C/Class01/pause.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    // Codigo
+    
+    printf("Hello World\n");
+    
+    system("pause"); // eh necessario clicar em uma tecla para encerrar o terminal
+}
